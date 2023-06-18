d7faff8a872ac7d0d72b05d94983520770d2ecc5
stephane-nguyen <stepdh01@gmail.com>
2023-06-18 11:54:12 +0200

    add back to top button

--- frontend-portfolio/src/pages/Contact.js
+++ frontend-portfolio/src/pages/Contact.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Typography, IconButton } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import SocialIcons from "../components/SocialIcons";
 
-import { Typography } from "@mui/material";
-
 export default function Contact() {
+  /* Back to top button */
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const shouldShow = scrollTop > 300; // Scroll Threshold from when the button appears
+    setShowBackToTop(shouldShow);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleBackToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <div id="contact">
@@ -13,6 +37,15 @@ export default function Contact() {
         </Typography>
         <SocialIcons />
       </div>
+      {showBackToTop && (
+        <IconButton
+          color="inherit"
+          onClick={handleBackToTop}
+          sx={{ position: "fixed", bottom: "16px", right: "16px" }}
+        >
+          <KeyboardArrowUpIcon />
+        </IconButton>
+      )}
     </>
   );
 }
