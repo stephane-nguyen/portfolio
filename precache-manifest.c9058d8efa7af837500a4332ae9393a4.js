adca979f04f338ee1d17bda274e275136901e2cb
Stéphane <stepdh01@gmail.com>
2023-06-20 22:51:41 +0200

    Updates

---
+++ precache-manifest.c9058d8efa7af837500a4332ae9393a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f1f426638a787055c53",
+    "url": "/portfolio/static/css/main.5e25d17f.chunk.css"
+  },
+  {
+    "revision": "0f1f426638a787055c53",
+    "url": "/portfolio/static/js/main.0f1f4266.chunk.js"
+  },
+  {
+    "revision": "ff6444d465728f11d6f1",
+    "url": "/portfolio/static/js/1.ff6444d4.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "85ba1c4f5434d38e3700e4daa8c14ba1",
+    "url": "/portfolio/static/media/resume.85ba1c4f.pdf"
+  },
+  {
+    "revision": "9e389d3b0653db4880f1c14cfb2c275b",
+    "url": "/portfolio/static/media/thales_logo.9e389d3b.png"
+  },
+  {
+    "revision": "c18170a119a21054ce4393f3f5a31800",
+    "url": "/portfolio/index.html"
+  }
+];
