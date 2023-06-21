84db5403678a0bd1c598330f7cfbfb0c2e93410e
stephane-nguyen <stepdh01@gmail.com>
2023-06-21 23:38:01 +0200

    add responsiveness for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { AppBar, Toolbar, Stack, Button, Typography } from "@mui/material";
-
+import { AppBar, Toolbar, Stack, Button, Typography, Box } from "@mui/material";
 import resume from "../images/resume.pdf";
-
 import { NavButton } from "./NavButton";
+import "../App.css";
 
 export default function Navbar() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" className="navbar">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Stéphane Nguyen
         </Typography>
-        <Stack direction="row" spacing={2} sx={{ marginLeft: "auto" }}>
-          <NavButton scrollTo={"about"} navSection={"About"} />
-          <NavButton scrollTo={"experiences"} navSection={"Experiences"} />
-          <NavButton scrollTo={"contact"} navSection={"Contact"} />
-          <NavButton scrollTo={"projects"} navSection={"Projects"} />
-
-          <Button color="inherit">
-            <a
-              className="button"
-              href={resume}
-              download="stephane_nguyen_resume.pdf"
-            >
-              Resume
-            </a>
-          </Button>
-        </Stack>
+        <Box sx={{ display: { xs: "none", md: "block" } }}>
+          <Stack direction="row" spacing={2}>
+            <NavButton scrollTo={"about"} navSection={"About"} />
+            <NavButton scrollTo={"experiences"} navSection={"Experiences"} />
+            <NavButton scrollTo={"contact"} navSection={"Contact"} />
+            <NavButton scrollTo={"projects"} navSection={"Projects"} />
+          </Stack>
+        </Box>
+        <Button color="inherit">
+          <a
+            className="button"
+            href={resume}
+            download="stephane_nguyen_resume.pdf"
+          >
+            Resume
+          </a>
+        </Button>
       </Toolbar>
     </AppBar>
   );
