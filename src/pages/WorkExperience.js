e1dac6d4d725e5fc6fabb8595256f6e8b92d8b41
stephane-nguyen <stepdh01@gmail.com>
2023-10-23 01:01:57 +0200

    Add work experience

--- src/pages/WorkExperience.js
+++ src/pages/WorkExperience.js
@@ -30,11 +30,13 @@ export default function WorkExperience() {
                 title="Full Stack Developer Apprentice"
                 date="March 2022 - September 2024"
                 location="Levallois-Perret, France"
-                description="Description 1"
+                description="Worked on internal tools to improve productivity and reduce manual processes of the users"
               />
             </Grid>
             <Grid item xs={4}>
-              <JobAccomplishmentCard text="Accomplished [X] as measured by [Y], by doing [Z]" />
+              <JobAccomplishmentCard text="Designed, implemented, unit tested, documented and deployed internal web applications to production using
+                SAPUI5, Node.js/Express, MongoDB, RESTful APIs and private cloud VMs."
+              />
             </Grid>
             <Grid item xs={1}>
               <TechnologyCard technology="Javascript" />
@@ -50,17 +52,21 @@ export default function WorkExperience() {
                 title="Cloud Software Tester and Automation Intern"
                 date="June 2023 - September 2023"
                 location="Palo Alto, CA, USA"
-                description="Description 3"
+                description="Worked on API performance testing and automation"
               />
             </Grid>
             <Grid item xs={4}>
-              <JobAccomplishmentCard text="Accomplished [X] as measured by [Y], by doing [Z]" />
+              <JobAccomplishmentCard text="Automated daily test analysis by creating Python scripts for statistical processing. 
+              Integrated them into the hotfix pipeline, enabling daily emails.
+                Created unit and integration tests in Python.
+                Leveraged JMeter to create test scenarios for evaluating API performance in
+                critical interactive use cases. 
+                Developed a Jenkins pipeline for automated test result reporting and to catch build issues." />
             </Grid>
             <Grid item xs={1}>
-              <TechnologyCard technology="Coming Soon" />
-              <TechnologyCard technology="Coming Soon" />
-              <TechnologyCard technology="Coming Soon" />
-              <TechnologyCard technology="Coming Soon" />
+              <TechnologyCard technology="Python" />
+              <TechnologyCard technology="Jenkins" />
+              <TechnologyCard technology="JMeter" />
             </Grid>
             <Grid item xs={4}>
               <JobCard
@@ -68,11 +74,10 @@ export default function WorkExperience() {
                 title="Applied mathematics Engineer Apprentice"
                 date="September 2021 - November 2021"
                 location="Gennevilliers, France"
-                description="Description 2"
               />
             </Grid>
             <Grid item xs={4}>
-              <JobAccomplishmentCard text="Accomplished [X] as measured by [Y], by doing [Z]" />
+              <JobAccomplishmentCard text="Provided a bibliographic study on a scheduling problem" />
             </Grid>
             <Grid item xs={1}>
               <TechnologyCard technology="Math research" />
