e5c70882245e35eda4a0a845c50cd4560a75e937
stephane-nguyen <stepdh01@gmail.com>
2023-10-23 00:37:12 +0200

    Update about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,28 +10,16 @@ export default function AboutMe() {
         </Typography>
 
         <Typography variant="body1" paragraph color="common.white">
-          Hello, nice to meet you! I'm Stéphane and I'm 21 years old. I come
-          from a math background, but I made a deliberate decision to switch my
-          major to Computer Science. I made this choice because I realized that
-          I have a strong affinity for programming, building applications,
-          problem-solving, and learning about the principles involved in
-          software engineering.
-        </Typography>
-
-        <Typography variant="body1" paragraph color="common.white">
-          Currently, I am working as a full stack developer at SAP, where I
-          contribute to the development of internal web applications. Being part
-          of a cross-engineering team, I provide support and collaborate with
-          other teams to create and enhance tools for improved efficiency.I am
+          Hello, nice to meet you! I'm Stéphane and I'm 22.
+          I am working as a full stack developer at SAP, where I contribute to the development of internal web applications. 
+          Being part of a cross-engineering team, I provide support and collaborate with other teams to create and enhance tools for improved efficiency. I am
           proficient in languages such as Python, Java, JavaScript.
         </Typography>
 
         <Typography variant="body1" paragraph color="common.white">
-          I find great satisfaction in utilizing my experience to make a
-          meaningful impact and positively influence as many people as possible.
-          Working alongside talented and driven colleagues is an environment in
-          which I thrive. If you would like to connect for a discussion, please
-          feel free to reach out to me.
+          I find great satisfaction in utilizing my experience to make a meaningful impact and positively influence as many people as possible. 
+          Working alongside talented and driven colleagues is an environment in which I thrive.
+          If you would like to connect for a discussion, please feel free to reach out to me.
         </Typography>
       </div>
     </>
